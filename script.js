cc3909aff6645eab7ba02d197d4113dc2465bfcd
Vallarasu <vallarasup245@gmail.com>
2024-12-27 05:13:52 +0530

    new commit

--- script.js
+++ script.js
@@ -109,7 +109,7 @@ body {
 }
 
 .certificate {
-    background-image: url(./background.jpeg);
+    background-image: url(./avsarts.jpeg);
     background-repeat: no-repeat;
     background-size: cover;
     width: 80%;
